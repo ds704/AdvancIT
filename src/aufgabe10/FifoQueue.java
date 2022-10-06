07f28622c273a5ca95d146edbfc94dfbdb7de8b2
ds704 <daniel.s704@outlook.com>
2022-10-06 16:17:26 +0200

    Der Versuch kam von Jonas aus dem Internet

--- src/aufgabe10/FifoQueue.java
+++ src/aufgabe10/FifoQueue.java
@@ -1,67 +1,57 @@
 package aufgabe10;
 
+import org.w3c.dom.Node;
+
 import java.sql.Array;
 import java.util.ArrayList;
 
 public class FifoQueue {
+    Node head;
+
     public static void main(String[] args) {
-        FifoQueue testList = new FifoQueue();
-        testList.put("Hallo");
-        testList.put("Daniel");
-        testList.put("Das Programm");
-        testList.put("Funktioniert :)");
+        FifoQueue neueListe = new FifoQueue();
+        neueListe = FifoQueue.Node.put(neueListe,"Hallo");
+        neueListe = FifoQueue.Node.put(neueListe,"Daniel");
+        neueListe = FifoQueue.Node.put(neueListe,"es hat geklappt");
+        FifoQueue.Node.get(neueListe);
 
-        System.out.println(testList.getLength()+" Es hat funktioniert! zEJ");
+    }
 
-        for(int i = 0; i < testList.getLenghOriginal(); i ++)
-        {
-            String ausgabe = testList.get();
-            System.out.println(ausgabe);
+    static class Node {
+        String data;
+        Node next;
+
+        Node(String d) {
+            data = d;
+            next = null;
         }
-        System.out.println(testList.getLength()+" Es hat funktioniert! zEJ");
-        System.out.println(testList.getLenghOriginal()+" Es hat funktioniert! zEJ");
 
-    }
+        public static FifoQueue put(FifoQueue list, String data) {
+            Node new_node = new Node(data);
+            if (list.head == null) {
+                list.head = new_node;
+            } else {
+                Node last = list.head;
+                while (last.next != null) {
+                    last = last.next;
+                }
 
-    //private String[] hauptspeicherStings = new Array[7];
-    //private String[] hauptspeicherStings = new Array[7];
-    private int objecteZaehler;
-    private ArrayList<String> hauptspeicherStrings = new ArrayList<String>();
+            }
+            return list;
+        }
 
-    /*public void FifoQueue ()
-    {
+            public static void get(FifoQueue list)
+            {
+                Node currNode = list.head;
+                System.out.print("Linked List: ");
+                while (currNode != null) {
+                    System.out.print(currNode.data + " ");
+                    currNode = currNode.next;
+                }
+            }
 
-    }*/
+
+        }
 
 
-    public void put(String s)
-    {
-        hauptspeicherStrings.add(s);
-        objecteZaehler++;
-    }
-
-    public String get()
-    {
-        String Ausgabe = hauptspeicherStrings.get(0);
-        /*String[] zwischenspeicher = new Array[objectZaehler-1];
-        for(int i = 1; i < objecteZaehler; i++)
-        {
-            zwischenspeicher[i-1] = hauptspeicherStrings.get(i);
-        }*/
-        hauptspeicherStrings.remove(0);
-        objecteZaehler--;
-        return Ausgabe;
-
-    }
-
-    public int getLength()
-    {
-        return objecteZaehler;
-
-    }
-
-    public int getLenghOriginal()
-    {
-        return hauptspeicherStrings.size();
-    }
 }

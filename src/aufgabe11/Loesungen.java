6d4e2cde888cb52353afdb73060480584541f20a
ds704 <daniel.s704@outlook.com>
2022-10-14 13:04:24 +0200

    Lösung von Pagnia ist leider nciht fertig. Zufinden ist die Lösung in seiner Lösung

---
+++ src/aufgabe11/Loesungen.java
@@ -0,0 +1,15 @@
+package aufgabe11;
+
+public class Loesungen extends Thread {
+    //das ist die Lösung von Herr Pagnia
+    public static final int THINKING = 0;
+    public static final int HUNGRY = 0;
+    public static final int EATING = 0;
+    int id = -1;
+
+    //static
+
+    public static void main(String[] args) {
+
+    }
+}

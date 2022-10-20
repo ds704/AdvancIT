a088aa2e9d2fdf3cedf96c48c096619e45f32237
ds704 <daniel.s704@outlook.com>
2022-10-20 12:49:46 +0200

    Funktioniert nicht

--- src/aufgabe12zEJ/Monitor.java
+++ src/aufgabe12zEJ/Monitor.java
@@ -1,7 +1,31 @@
 package aufgabe12zEJ;
 
 public class Monitor {
+
+    public int Hallo =123;
     public static void main(String[] args) {
 
+    }
+
+    public void essenStarten(int id) throws InterruptedException {
+        if(Philosophen.ZustandPhilosophen[(id+4)%5] != Philosophen.ESSEND && Philosophen.ZustandPhilosophen[(id+1)%5] != Philosophen.ESSEND)
+        {
+            Philosophen.ZustandPhilosophen[id] = Philosophen.ESSEND;
+            System.out.println("Philosoph mit der id: " + id + " isst gerade!");
+        }else {
+            Philosophen.ZustandPhilosophen[id] = Philosophen.WARTEND;
+            Philosophen.PhilosophenArray[id].wait();
+        }
+
+    }
+    private void essenStoppen(int id)
+    {
+        Philosophen.ZustandPhilosophen[id] = Philosophen.DENKEND;
+        if(Philosophen.ZustandPhilosophen[(id+4)%5] == Philosophen.WARTEND)
+        {
+            Philosophen.PhilosophenArray[(id+4)%5].notify();
+        }else if(Philosophen.ZustandPhilosophen[(id+1)%5] == Philosophen.WARTEND) {
+        Philosophen.PhilosophenArray[(id+4)%5].notify();
+    }
     }
 }
--- src/aufgabe12zEJ/Philosophen.java
+++ src/aufgabe12zEJ/Philosophen.java
@@ -1,7 +1,47 @@
 package aufgabe12zEJ;
 
 public class Philosophen extends Thread{
-    public static void main(String[] args) {
 
+    private int id;
+    static Philosophen PhilosophenArray[] = new Philosophen[5];
+    //static int count = 0;
+    protected static int[] ZustandPhilosophen = new int[5];
+
+    protected static int DENKEND = 0;
+    protected static int ESSEND = 1;
+    protected static int WARTEND = 2;
+    public static Monitor neuerMonitor = new Monitor();
+
+
+    public static void main(String[] args) {
+        Monitor neuerMonitor = new Monitor();
+        for (int i = 0; i < 5 ; i++) {
+            PhilosophenArray[i] = new Philosophen(i);
+            PhilosophenArray[i].start();
+
+
+        }
+
+    }
+
+    public Philosophen(int id)
+    {
+        this.id = id;
+    }
+
+
+    public void run()
+    {
+        try{
+            while(true) {
+                    neuerMonitor.essenStarten(id);
+                Thread.sleep(1000);
+
+            }
+
+        }catch(Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }

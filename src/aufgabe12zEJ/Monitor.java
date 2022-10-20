bec9448a9797ab4f81a1172c8b1a4a49ed818dc1
ds704 <daniel.s704@outlook.com>
2022-10-20 11:18:18 +0200

    Neue Aufgaben ohne Code

---
+++ src/aufgabe12zEJ/Monitor.java
@@ -0,0 +1,7 @@
+package aufgabe12zEJ;
+
+public class Monitor {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/aufgabe12zEJ/Philosophen.java
@@ -0,0 +1,7 @@
+package aufgabe12zEJ;
+
+public class Philosophen extends Thread{
+    public static void main(String[] args) {
+
+    }
+}
